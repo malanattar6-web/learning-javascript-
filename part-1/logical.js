6343da9f7b41be465174fdc258c6df4a97ed07c3
malan attar <malanattar6@gmail.com>
2026-02-26 09:55:00 +0530

    logical

---
+++ part-1/logical.js
@@ -0,0 +1,13 @@
+//&&->and
+//||->or
+//!_>reverse
+
+let isLoggedIn = true;
+let ispaid =false;
+
+console.log(isLoggedin && ispaid);
+
+let isEmailLuser = true;
+let isGoogleuser = false;
+
+console.log(isEmailuser || isGoogleuser);
